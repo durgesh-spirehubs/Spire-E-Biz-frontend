a200d05bce62922b24ae6b97f65401280c753331
Durgesh <durgesh@spirehubs.com>
2026-01-06 12:30:30 +0530

    custome details added addresses

--- src/app/(routes)/customers/[customerID]/page.tsx
+++ src/app/(routes)/customers/[customerID]/page.tsx
@@ -2,6 +2,7 @@
 import { useParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import axiosServices from '@/lib/axios'
+import Loader from '@/components/ui/loader'
 import CustomerDetails from '@/view/customer/customerDetails'
 const CustomerDetailsPage = () => {
   const [currentData, setCurrentData] = useState(undefined);
@@ -9,11 +10,10 @@ const CustomerDetailsPage = () => {
   const {customerID}=useParams();
   useEffect(() => {
     if (!customerID) return;
-    const controller = new AbortController();
     const fetchSingleData = async () => {
       setIsLoading(true);
       try {
-        const response = await axiosServices.get(`/api/customers/${customerID}`, { signal: controller.signal });
+        const response = await axiosServices.get(`/api/customers/${customerID}`);
         setCurrentData(response?.data?.data);
       } catch (err) {
         if ((err as any)?.code === 'ERR_CANCELED') return;
@@ -23,7 +23,6 @@ const CustomerDetailsPage = () => {
       }
     };
     fetchSingleData();
-    return () => controller.abort();
   }, [customerID])
 
   if (isLoading) {
@@ -32,11 +31,10 @@ const CustomerDetailsPage = () => {
     //     <CircularProgress size={50} />
 
     //   </Stack>
-    <div>Loading </div>
+      <Loader/>
     )
   } else {
     return <CustomerDetails data={currentData}  />
   }
 }
-CustomerDetailsPage.acl = ['Admin', 'Customer']
 export default CustomerDetailsPage;
